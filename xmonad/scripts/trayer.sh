381d1370741461cebe9b96b9a3f7b688de601154
Yaroslav Novitsky <rcr3211@gmail.com>
2023-12-25 13:58:32 +0300

    fix: trayer spacing;

--- xmonad/scripts/trayer.sh
+++ xmonad/scripts/trayer.sh
@@ -3,4 +3,4 @@ trayer --transparent true --edge top \
     --align right --SetDockType true \
     --SetPartialStrut true --expand true \
     --width 10 --height 21 --alpha 56 \
-    --tint 0 --iconspacing 4
+    --tint 0
